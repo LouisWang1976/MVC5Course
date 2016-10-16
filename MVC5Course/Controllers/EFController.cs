b19e572fa926aa16c211abfb29360f88d7affab5
kamiu <kamiu65@gmail.com>
2016-10-16 16:30:01 +0800

    25 練習對 Entity Framework 的資料存取程式進行效能調校

--- MVC5Course/Controllers/EFController.cs
+++ MVC5Course/Controllers/EFController.cs
@@ -1,4 +1,5 @@
 ﻿using MVC5Course.Models;
+using MVC5Course.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity.Validation;
@@ -77,20 +78,36 @@ namespace MVC5Course.Controllers
         }
         public ActionResult Add5Percernt()
         {
-            var data = db.Product.Where(p => p.ProductName.Contains("White"));
-            foreach(var temp in data)
-            {
-                if(temp.Price.HasValue)
-                {
-                    temp.Price = temp.Price * 1.05m;
-                }
-            }
-            db.SaveChanges();
+            string ProductName = "%White%";
+            db.Database.ExecuteSqlCommand("update dbo.product set Price=Price*0.95 where ProductName like @p0", ProductName);
+            //var data = db.Product.Where(p => p.ProductName.Contains("White"));
+            //foreach(var temp in data)
+            //{
+            //    if(temp.Price.HasValue)
+            //    {
+            //        temp.Price = temp.Price * 1.05m;
+            //    }
+            //}
+            //db.SaveChanges();
             return RedirectToAction("Index");
         }
         public ActionResult ClientContribution()
         {
             var data = db.vw_ClientContribution.Take(10);
+            return View(data);
+        }
+        public ActionResult ClientContributionView()
+        {
+            string FirstName = "%mary%";
+          var data=  db.Database.SqlQuery<ClientContributionView>(@"	SELECT
+         c.ClientId,
+         c.FirstName,
+         c.LastName,
+         (SELECT SUM(o.OrderTotal)
+          FROM[dbo].[Order] o
+          WHERE o.ClientId = c.ClientId) as OrderTotal
+    FROM
+        [dbo].[Client] as c where FirstName like @p0" , FirstName);
             return View(data);
         }
     }
---
+++ MVC5Course/Models/ViewModels/ClientContributionView.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MVC5Course.Models.ViewModels
+{
+    public class ClientContributionView
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Nullable<decimal> OrderTotal { get; set; }
+    }
+}
